58a07451a9919fe0ab0739f0c6bf2de8fcf707c6
Zack Gilbert <zackgilbert@gmail.com>
2013-11-16 17:02:51 -0600

    Rails admin + pages controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def is_admin?
+    ['zack@outpostqa.com', 'mark@outpostqa.com', 'admin@outpostqa.com'].include? email
+  end
+
 end
