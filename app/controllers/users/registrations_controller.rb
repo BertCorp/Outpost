2972c909d9e7132b7b7a1035fa9fb64dafed60d4
Zack Gilbert <zackgilbert@gmail.com>
2013-11-17 17:40:18 -0600

    Setup basic login and registration.

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,64 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_params
+
+  def new   
+    build_resource({})
+    respond_with self.resource
+  end
+
+  def create
+    @company = Company.find_or_create_by(name: params[:user][:company])
+    params[:user][:company_id] = @company.id
+    
+    super
+  end
+  
+  def edit
+    @user = User.find(current_user.id)
+    super
+  end
+
+  # Override the devise update method for registrations
+  def update
+    @user = User.find(current_user.id)
+
+    # See https://github.com/plataformatec/devise/wiki/How-To%3A-Allow-users-to-edit-their-account-without-providing-a-password
+    successfully_updated = if needs_password?(@user, params)
+      @user.update_with_password(devise_parameter_sanitizer.for(:account_update))
+    else
+      params[:user].delete(:current_password)
+      
+      @user.update_without_password(devise_parameter_sanitizer.for(:account_update))
+    end
+
+    if successfully_updated
+      sign_in @user, :bypass => true
+      redirect_to root_path, notice: 'User was successfully updated.'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  
+    # check if we need password to update user data
+    # ie if password or email was changed
+    # extend this as needed
+    def needs_password?(user, params)
+      #user.email != params[:user][:email] ||
+        params[:user][:password].present?
+    end
+
+    def configure_permitted_params
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(
+          :name, :email, :company_id, :password, :password_confirmation
+        )
+      end
+      devise_parameter_sanitizer.for(:account_update) do |u|
+        u.permit(
+          :name, :email, :company_id, :password, :password_confirmation, :current_password
+        )
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,18 @@ Outpost::Application.routes.draw do
     mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   end
   
-  devise_for :users, :path => "", :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' },
+    controllers: {
+      registrations: 'users/registrations'
+    }
+
+  devise_scope :user do
+    get "/logout" => "devise/sessions#destroy"
+    
+    #authenticated :user do
+    #  root to: 'users#dashboard', as: :authenticated_root
+    #end
+  end  
   
   root "pages#index"
   
