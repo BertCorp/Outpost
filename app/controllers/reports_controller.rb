f5984b4028be3f2107ab8ce7f8278f0d369476d1
Mark Bertrand <marksbertrand@gmail.com>
2013-11-20 19:06:47 -0800

    Cleaning up form inputs

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -69,6 +69,6 @@ class ReportsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def report_params
-      params.require(:report).permit(:company_id, :test_suite_id, :initiated_at, :initiated_by, :started_at, :completed_at, :monitored_by, :status, :summary)
+      params.require(:report).permit(:company_id, :test_suite_id, :initiated_at, :initiated_by, :started_at, :completed_at, :monitored_by, :status, :summary, :created_at, :updated_at)
     end
 end
--- app/controllers/test_cases_controller.rb
+++ app/controllers/test_cases_controller.rb
@@ -71,6 +71,6 @@ class TestCasesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def test_case_params
-      params.require(:test_case).permit(:company_id, :test_suite_id, :title, :description, :setup_started_at, :setup_completed_at, :pending_message, :created_at, :updated_at)
+      params.permit(:company_id, :test_suite_id, :title, :description, :setup_started_at, :setup_completed_at, :pending_message, :created_at, :updated_at)
     end
 end
