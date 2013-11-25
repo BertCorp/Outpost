15aa8039ee8ceb4f451fa342e881e1144eea9ec4
Mark Bertrand <marksbertrand@gmail.com>
2013-11-25 14:20:20 -0800

    Fixing report show page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,7 +10,7 @@ class ReportsController < ApplicationController
   # GET /reports/1
   # GET /reports/1.json
   def show
-    @test_case = @report.test_cases
+    @results = @report.results
   end
 
   # GET /reports/new
