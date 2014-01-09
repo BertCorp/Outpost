d2c345c223cdfcb109549812b002d2e707375241
Zack Gilbert <zackgilbert@gmail.com>
2014-01-08 18:21:56 -0600

    Clean up some more admin stuff.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def urlify(s)
+    s.gsub(%r{\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))}u) do |s|
+      %Q{<a href="#{s}">#{s}</a>}
+    end.html_safe
+  end
+  
 end
