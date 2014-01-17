3b2818de52cd82a079b07ee91f916baef564e6b3
Zack Gilbert <zackgilbert@gmail.com>
2014-01-17 13:49:41 -0600

    Better helper for html-ify-ing text blocks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+  def htmlify(s)
+    return simple_format(urlify(s)) if s.present?
+    ''
+  end
+  
   def urlify(s)
     s.gsub(%r{\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))}u) do |s|
       %Q{<a href="#{s}">#{s}</a>}
