202f000624aadeb6fce5a87f3ef07af0562f40fd
Zack Gilbert <zackgilbert@gmail.com>
2014-04-24 16:41:26 -0500

    Clean up tests folder. Delete unneeded files.

--- tests/Test Client/test_suite.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ruby
-
-#require File.expand_path(File.dirname(__FILE__) + "/1 - test.rb")
-Dir.glob(File.dirname(__FILE__) + "/*.rb").sort.each do |file| 
-  require file unless ['test_suite.rb', 'client_variables.rb'].include? File.basename(file)
-end
